40f78dfb4dedfb101d3c0e73120eced537dc2013
Nathalie Maes <nathalie@pau.be>
2017-09-13 10:10:54 +0200

    Fix spelling error.

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
         component: PoketeamComponent
     },
     {
-        path: 'suggestons',
+        path: 'suggestions',
         component: SuggestiesComponent
     }
 ];
